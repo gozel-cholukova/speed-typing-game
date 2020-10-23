a3007dbd7a10b4e910de10f3775b8cdf724cabe1
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-10-24 00:07:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//globals
+let time = 5;
+let scpre = 0;
+let isPlaying;
+
+//dom elements
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scorDisplay = docuemnt.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
